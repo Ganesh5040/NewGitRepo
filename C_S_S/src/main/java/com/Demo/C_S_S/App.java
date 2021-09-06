fb1c12b9e413e0066cf8bd67b4191b91fc1fc1ea
Ganesh5040 <ganeshpawale1990@gmail.com>
2021-09-06 15:51:47 +0530

    first commit

---
+++ C_S_S/src/main/java/com/Demo/C_S_S/App.java
@@ -0,0 +1,13 @@
+package com.Demo.C_S_S;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ C_S_S/src/test/java/com/Demo/C_S_S/MyTest.java
@@ -0,0 +1,17 @@
+package com.Demo.C_S_S;
+
+public class MyTest {
+
+	public void m1() {
+		
+	}
+	
+	public void m2() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
